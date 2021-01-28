8fe6b1a49c7a8af71a73c7a75ffac45935f1af9f
mjones1818 <michael.jones0532@gmail.com>
2021-01-28 10:45:07 -0600

    added restaurant info display

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let loginName = document.getElementById('name')
 let neighborhoodsDropdown = document.getElementById('neighborhoods')
 let restaurantSearch = document.getElementById('restaurant-search')
 let restaurantResults = document.getElementById('restaurant-results')
+let restaurantInfo = document.getElementById('restaurant-info')
+let restaurantInfoPopup = document.getElementById('restaurant-info-popup')
 let cuisinesArray = [] 
 let userObj = {}
 // let wordlist = cuisinesArray
@@ -25,6 +27,7 @@ loginName.addEventListener('keyup', function(event){
   }
 })
 restaurantResults.addEventListener('click', handleRestaurantSelection)
+restaurantInfoPopup.addEventListener('click', handleRestaurantButtons)
 
 function handleLoginSubmit(e) {
   fetch('http://localhost:3000/users',{
@@ -119,7 +122,7 @@ function populateRestaurantList(restaurants)  {
   restaurantResults.innerHTML = ''
   restaurants.forEach(function(restaurant){
     restaurantResults.innerHTML += `
-      <div data-id=>
+      <div data-id=${restaurant.restaurant.db_id}>
         <h3>${restaurant.restaurant.name}</h3>
         <p>Average cost for two people: $${restaurant.restaurant.average_cost_for_two}</p>
         Rating: ${restaurant.restaurant.user_rating.aggregate_rating}
@@ -130,15 +133,43 @@ function populateRestaurantList(restaurants)  {
 
 
 function handleRestaurantSelection(e) {
-  document.body.innerHTML += `
-  <div class="center">
-  </div>
-  <div class="popup-overlay"></div>
-  <div class="popup">
-  <p>I'm baby organic pork belly tofu venmo, roof party seitan artisan portland. Sriracha typewriter microdosing truffaut. Food truck kale chips bitters, locavore stumptown fashion axe +1 snackwave. Hell of adaptogen bicycle rights man bun, craft beer +1 typewriter vinyl williamsburg four loko gentrify. You probably haven't heard of them tumblr tote bag photo booth tacos chicharrones quinoa microdosing wayfarers intelligentsia fashion axe poke pinterest tofu disrupt.
+  
+  let dbId = e.target.dataset.id
+  fetch(`http://localhost:3000/restaurants/${dbId}`)
+  .then(function(resp){
+    return resp.json()
+  })
+  .then(function(restaurant){
+    console.log(restaurant)
+    populateRestaurantInfo(restaurant)
+  })
+  
 
-  Meggings letterpress listicle woke godard. You probably haven't heard of them affogato cloud bread, readymade brooklyn waistcoat viral. Try-hard pop-up live-edge, umami cray crucifix coloring book. Meh tattooed cornhole flannel keytar offal adaptogen lomo photo booth synth schlitz.</p>
-  </div>
+}
+
+function populateRestaurantInfo(restaurant){
+  restaurantInfoPopup.innerHTML += `
+
+      <h3>${restaurant.name}</h3>
+      <h5>${restaurant.locality}</h5>
+      <p>${restaurant.address}</p>
+      Average cost for two: $${restaurant.average_cost_for_two} <br>
+      <img src="${restaurant.thumb}"> <br>
+      <button id='go-back'>Go back</button>
+      <button id='like'>Like this Restaurant</button>
+
   `
-  // openLoginForm()
+  restaurantInfo.classList.add('restaurant-popup', 'popup')
+  restaurantInfo.style.width = '50%'
+  restaurantInfo.style.height = '60%'
+  document.getElementsByClassName('popup-overlay')[0].style.display = 'block'
+}
+
+
+function handleRestaurantButtons(e) {
+  if (e.target.id === 'go-back') {
+    restaurantInfo.classList.remove('restaurant-popup', 'popup')
+    document.getElementsByClassName('popup-overlay')[0].style.display = 'none'
+    restaurantInfoPopup.innerHTML = ''
+  }
 }
